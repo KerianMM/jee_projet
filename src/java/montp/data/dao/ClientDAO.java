9449b639edbea608b465d0545372a9c876331a97
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-12-04 15:08:44 +0100

    new (service) clients by current user

--- src/java/montp/data/dao/ClientDAO.java
+++ src/java/montp/data/dao/ClientDAO.java
@@ -1,12 +1,20 @@
 package montp.data.dao;
 
 import montp.data.entity.ClientEntity;
+import montp.data.entity.PersonEntity;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class ClientDAO extends GenericDAO<ClientEntity> {
     public ClientDAO() {
         super(ClientEntity.class);
+    }
+
+    public List<ClientEntity> findByOwner(PersonEntity personEntity) {
+        return em.createQuery(makeQLString("c", "WHERE c.person = :person"))
+                .setParameter("person", personEntity)
+                .getResultList();
     }
 }
--- src/java/montp/services/ClientService.java
+++ src/java/montp/services/ClientService.java
@@ -2,8 +2,14 @@ package montp.services;
 
 import montp.data.dao.ClientDAO;
 import montp.data.entity.ClientEntity;
+import montp.data.entity.security.UserEntity;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
-public class ClientService extends GenericService<ClientEntity, ClientDAO> {}
+public class ClientService extends GenericService<ClientEntity, ClientDAO> {
+    public List<ClientEntity> getByCurrentUser(UserEntity userEntity) {
+        return dao.findByOwner(userEntity.getPerson());
+    }
+}
