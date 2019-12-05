b08e55e519dbe1b346313545f560b1810707c246
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-12-05 10:22:51 +0100

    new (dataAccess) dao and service for projectEntity

---
+++ src/java/montp/data/dao/ProjectDAO.java
@@ -0,0 +1,5 @@
+package montp.data.dao;
+
+import montp.data.entity.ProjectEntity;
+
+public class ProjectDAO extends GenericDAO<ProjectEntity> {}
---
+++ src/java/montp/services/ProjectService.java
@@ -0,0 +1,6 @@
+package montp.services;
+
+import montp.data.dao.ProjectDAO;
+import montp.data.entity.ProjectEntity;
+
+public class ProjectService extends GenericService<ProjectEntity, ProjectDAO> {}
