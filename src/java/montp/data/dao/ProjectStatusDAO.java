84969fcd6cb105cd71659f5b43d38168208f4943
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-12-05 10:18:19 +0100

    new (dataAccess) dao and service for projectStatusEntity

---
+++ src/java/montp/data/dao/ProjectStatusDAO.java
@@ -0,0 +1,5 @@
+package montp.data.dao;
+
+import montp.data.entity.ProjectStatusEntity;
+
+public class ProjectStatusDAO extends GenericDAO<ProjectStatusEntity> {}
---
+++ src/java/montp/services/ProjectStatusService.java
@@ -0,0 +1,6 @@
+package montp.services;
+
+import montp.data.dao.ProjectStatusDAO;
+import montp.data.entity.ProjectStatusEntity;
+
+public class ProjectStatusService extends GenericService<ProjectStatusEntity, ProjectStatusDAO> {}
