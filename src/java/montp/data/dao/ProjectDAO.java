34ce6e9b3c006879509740407026176b8514cf0e
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-12-05 10:59:24 +0100

    fix (scope) application scoped

--- src/java/montp/data/dao/ProjectDAO.java
+++ src/java/montp/data/dao/ProjectDAO.java
@@ -2,4 +2,7 @@ package montp.data.dao;
 
 import montp.data.entity.ProjectEntity;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class ProjectDAO extends GenericDAO<ProjectEntity> {}
--- src/java/montp/services/ProjectStatusService.java
+++ src/java/montp/services/ProjectStatusService.java
@@ -3,4 +3,7 @@ package montp.services;
 import montp.data.dao.ProjectStatusDAO;
 import montp.data.entity.ProjectStatusEntity;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class ProjectStatusService extends GenericService<ProjectStatusEntity, ProjectStatusDAO> {}
