3501c7cd0262ada6e4e82f86ca5ccb94a1bb4e8f
Kérian MONTES-MORIN <kerianmontes@gmail.com>
2019-12-06 11:24:08 +0100

    update (dao) paymentMode findAll order

--- src/java/montp/data/dao/PayementModeDAO.java
+++ src/java/montp/data/dao/PayementModeDAO.java
@@ -3,10 +3,17 @@ package montp.data.dao;
 import montp.data.entity.PayementModeEntity;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
 
 @ApplicationScoped
 public class PayementModeDAO extends GenericDAO<PayementModeEntity> {
     public PayementModeDAO() {
         super(PayementModeEntity.class);
+    }
+
+    @Override
+    public List<PayementModeEntity> findAll() {
+        return em.createQuery(makeQLString("s", "ORDER BY s.orderNum"))
+                .getResultList();
     }
 }
